9c99faf29e23ebfd5d90b9546d0c311022bd764a
Akshay <araa@connect.ust.hk>
2024-08-21 20:31:36 +0800

    Fix brightness adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -60,6 +60,7 @@ void reconnectWebSocket();
 void printMemoryUsage();
 void processQueuedMessages();
 void queueWebSocketMessage(uint8_t* payload, size_t length);
+void updateButtonStates();
 
 // Global variables
 unsigned long lastDebounceTime[ROWS][COLS] = {{0}};
@@ -492,28 +493,41 @@ void buttonCheckTask(void * parameter) {
         if ((upButtonPressed || downButtonPressed) && (millis() - lastBrightnessAdjustTime > BRIGHTNESS_ADJUST_INTERVAL)) {
             SERIAL_PRINTLN("Entering brightness adjustment block");
             isBrightnessUpdateInProgress = true;
-            bool timeoutReached = false;
-            for (int y = 0; y < ROWS; y++) {
-                for (int x = 0; x < COLS; x++) {
-                    if (buttonState[y][x] && !(x == UP_BUTTON_X && y == UP_BUTTON_Y) && !(x == DOWN_BUTTON_X && y == DOWN_BUTTON_Y)) {
-                        SERIAL_PRINTF("Calling adjustBrightness for button at (%d, %d)\n", x, y);
-                        timeoutReached = adjustBrightness(x, y, upButtonPressed);
-                        if (timeoutReached) {
-                            break;
+            unsigned long adjustmentStartTime = millis();
+
+            while ((upButtonPressed || downButtonPressed) && (millis() - adjustmentStartTime <= BRIGHTNESS_UPDATE_TIMEOUT_MS)) {
+                for (int y = 0; y < ROWS; y++) {
+                    for (int x = 0; x < COLS; x++) {
+                        if (buttonState[y][x] && !(x == UP_BUTTON_X && y == UP_BUTTON_Y) && !(x == DOWN_BUTTON_X && y == DOWN_BUTTON_Y)) {
+                            SERIAL_PRINTF("Calling adjustBrightness for button at (%d, %d)\n", x, y);
+                            adjustBrightness(x, y, upButtonPressed);
                         }
-                        // Add a small delay to prevent overwhelming the system
-                        vTaskDelay(pdMS_TO_TICKS(10));
                     }
                 }
-                if (timeoutReached) {
-                    break;
-                }
+
+                // Add a small delay to prevent overwhelming the system
+                vTaskDelay(pdMS_TO_TICKS(10));
+
+                // Update button states
+                updateButtonStates();
             }
+
             lastBrightnessAdjustTime = millis();
             isBrightnessUpdateInProgress = false;
-            if (timeoutReached) {
+            if (millis() - adjustmentStartTime > BRIGHTNESS_UPDATE_TIMEOUT_MS) {
+                SERIAL_PRINTLN("Brightness adjustment timeout reached");
                 isBrightnessAdjustmentMode = false;
                 restoreStates();
+            } else {
+                // Finalize the brightness adjustment
+                for (int i = 0; i < NUM_MAPPINGS; i++) {
+                    if (entityMappings[i].x == lastAdjustedX && entityMappings[i].y == lastAdjustedY) {
+                        SERIAL_PRINTF("Sending final brightness update for entity at (%d, %d)\n", lastAdjustedX, lastAdjustedY);
+                        sendBrightnessUpdate(entityMappings[i].entity_id, currentAdjustmentBrightness);
+                        entityStates[lastAdjustedY][lastAdjustedX].brightness = currentAdjustmentBrightness;
+                        break;
+                    }
+                }
             }
             SERIAL_PRINTLN("Exiting brightness adjustment block");
         } else if (!upButtonPressed && !downButtonPressed && isBrightnessAdjustmentMode) {
@@ -547,7 +561,9 @@ void buttonCheckTask(void * parameter) {
 
 bool adjustBrightness(int x, int y, bool increase) {
     SERIAL_PRINTF("Entering adjustBrightness: x=%d, y=%d, increase=%d\n", x, y, increase);
-    bool timeoutReached = false;
+    static unsigned long lastAdjustmentTime = 0;
+    const unsigned long ADJUSTMENT_INTERVAL = 50; // Adjust every 50ms for smoother transitions
+
     if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE) {
         SERIAL_PRINTLN("Mutex acquired in adjustBrightness");
         for (int i = 0; i < NUM_MAPPINGS; i++) {
@@ -563,24 +579,22 @@ bool adjustBrightness(int x, int y, bool increase) {
                     lastAdjustedY = y;
                 }
 
-                if (increase) {
-                    currentAdjustmentBrightness = min(255, currentAdjustmentBrightness + BRIGHTNESS_STEP);
-                } else {
-                    currentAdjustmentBrightness = max(0, currentAdjustmentBrightness - BRIGHTNESS_STEP);
-                }
-                SERIAL_PRINTF("Adjusted brightness to %d\n", currentAdjustmentBrightness);
+                // Only adjust brightness if enough time has passed since the last adjustment
+                if (millis() - lastAdjustmentTime >= ADJUSTMENT_INTERVAL) {
+                    if (increase) {
+                        currentAdjustmentBrightness = min(255, currentAdjustmentBrightness + BRIGHTNESS_STEP);
+                    } else {
+                        currentAdjustmentBrightness = max(0, currentAdjustmentBrightness - BRIGHTNESS_STEP);
+                    }
+                    SERIAL_PRINTF("Adjusted brightness to %d\n", currentAdjustmentBrightness);
 
-                // Pass the color information to displayBrightnessLevel
-                displayBrightnessLevel(currentAdjustmentBrightness, 
-                                       entityStates[y][x].r, 
-                                       entityStates[y][x].g, 
-                                       entityStates[y][x].b);
+                    // Pass the color information to displayBrightnessLevel
+                    displayBrightnessLevel(currentAdjustmentBrightness, 
+                                           entityStates[y][x].r, 
+                                           entityStates[y][x].g, 
+                                           entityStates[y][x].b);
 
-                if (millis() - brightnessAdjustmentStartTime > BRIGHTNESS_ADJUST_TIMEOUT) {
-                    SERIAL_PRINTLN("Brightness adjustment timeout reached");
-                    sendBrightnessUpdate(entityMappings[i].entity_id, currentAdjustmentBrightness);
-                    entityStates[y][x].brightness = currentAdjustmentBrightness;
-                    timeoutReached = true;
+                    lastAdjustmentTime = millis();
                 }
 
                 break;
@@ -592,7 +606,7 @@ bool adjustBrightness(int x, int y, bool increase) {
         SERIAL_PRINTLN("Failed to acquire mutex in adjustBrightness");
     }
     SERIAL_PRINTLN("Exiting adjustBrightness");
-    return timeoutReached;
+    return false; // We're no longer checking for timeout here
 }
 
 void updateTimeAndCheckNightMode(const char* time_str) {
@@ -790,5 +804,39 @@ void processQueuedMessages() {
         xSemaphoreGive(queueMutex);
     } else {
         SERIAL_PRINTLN("Failed to acquire queue mutex in processQueuedMessages");
+    }
+}
+
+// Add this new function to update button states
+void updateButtonStates() {
+    for (int y = 0; y < ROWS; y++) {
+        pinMode(rowPins[y], OUTPUT);
+        digitalWrite(rowPins[y], LOW);
+
+        for (int x = 0; x < COLS; x++) {
+            pinMode(colPins[x], INPUT_PULLUP);
+            bool reading = digitalRead(colPins[x]) == LOW;
+
+            if (reading != lastButtonState[y][x]) {
+                lastDebounceTime[y][x] = millis();
+            }
+
+            if ((millis() - lastDebounceTime[y][x]) > DEBOUNCE_TIME) {
+                if (reading != buttonState[y][x]) {
+                    buttonState[y][x] = reading;
+
+                    if (x == UP_BUTTON_X && y == UP_BUTTON_Y) {
+                        upButtonPressed = buttonState[y][x];
+                    } else if (x == DOWN_BUTTON_X && y == DOWN_BUTTON_Y) {
+                        downButtonPressed = buttonState[y][x];
+                    }
+                }
+            }
+
+            lastButtonState[y][x] = reading;
+            pinMode(colPins[x], INPUT);
+        }
+
+        pinMode(rowPins[y], INPUT);
     }
 }
